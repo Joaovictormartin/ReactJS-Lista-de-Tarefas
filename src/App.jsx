bb06c26888663aee83bf9bdf4dde660ffad4f993
Joao <joao.teixeira@aedb.br>
2021-09-13 09:30:33 -0300

    adicionando o component taskList no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
 import { Header } from './components/Header'
+import { TaskList } from './components/TaskList'
 import './styles/global.scss'
 
 export function App() {
   return(
-    <Header/>
+    <>
+      <Header/>
+      <TaskList/>
+    </>
   )
 }
