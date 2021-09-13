5ebc895cc0793f71d88aeb41c7f469a286f3fae0
Joao <joao.teixeira@aedb.br>
2021-09-13 00:15:32 -0300

    criando o Header do projeto

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '../styles/header.scss'
+import img from '../assets/img/imgHeader.png'
+
+export function Header() {
+  return(
+    <header className="header">
+      <div>
+        <img src='' alt="" />
+      </div>
+    </header>
+  )
+}
